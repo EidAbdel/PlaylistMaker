da28beb3796340744968ba872dee40344cc98eca
Абдель <AR.Eid@rendez-vous.ru>
2024-08-12 10:22:33 +0300

    Снес проект и воостановил заново чтобы в пул реквесте был полный код. Убрал все что было после 8 спринта.

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,24 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        val bt_search = findViewById<Button>(R.id.find_bt)
+        val bt_media = findViewById<Button>(R.id.media_bt)
+        val bt_setting = findViewById<Button>(R.id.setting_bt)
+
+        bt_setting.setOnClickListener{
+            val displayIntent = Intent(this, MainSetting::class.java)
+            startActivity(displayIntent)
+        }
+        bt_media.setOnClickListener{
+            val displayIntent = Intent(this, MediaActivity::class.java)
+            startActivity(displayIntent)
+        }
+        bt_search.setOnClickListener{
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
         }
     }
 }
---
+++ app/src/main/java/com/practicum/playlistmaker/MainSetting.kt
@@ -0,0 +1,23 @@
+package com.practicum.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainSetting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_setting)
+
+        val bt_back_main_menu = findViewById<Button>(R.id.back_setting_bt)
+        bt_back_main_menu.setOnClickListener {
+            val displayIntent = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent)
+        }
+    }
+}
