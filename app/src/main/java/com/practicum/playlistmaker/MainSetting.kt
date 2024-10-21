88ebcf0feed6309c2241130560e6fc4a339c210d
Абдель <AR.Eid@rendez-vous.ru>
2024-10-21 10:24:50 +0300

    ДЗ спринт 12 (переклчение темы) Переключение и отслеживание темы на устройстве

--- app/src/main/java/com/practicum/playlistmaker/MainSetting.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainSetting.kt
@@ -1,12 +1,15 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class MainSetting : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +21,15 @@ class MainSetting : AppCompatActivity() {
         val btShare = findViewById<Button>(R.id.bt_share)
         val btSupport = findViewById<Button>(R.id.bt_support)
         val btUserAgreement = findViewById<Button>(R.id.bt_user_agreement)
+        val themeSwitcher = findViewById<SwitchMaterial>(R.id.themeSwitcher)
+
+        themeSwitcher.isChecked = resources.configuration.uiMode and
+                Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+
+        themeSwitcher.setOnCheckedChangeListener { switcher, checked ->
+            (applicationContext as App).switchTheme(checked)
+        }
+
 
         btBackMainMenu.setOnClickListener {
             finish()
@@ -26,7 +38,7 @@ class MainSetting : AppCompatActivity() {
         btShare.setOnClickListener {
             val shareIntent = Intent(Intent.ACTION_SEND)
             shareIntent.setType("text/plain")
-            shareIntent.putExtra(Intent.EXTRA_TEXT,getString(R.string.share_link) )
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_link))
             startActivity(Intent.createChooser(shareIntent, null))
         }
 
@@ -41,7 +53,8 @@ class MainSetting : AppCompatActivity() {
         }
 
         btUserAgreement.setOnClickListener {
-            val agreementIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.user_agreement_link)))
+            val agreementIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.user_agreement_link)))
             startActivity(agreementIntent)
         }
 
