6d25d4f3aa96084035d365cb45e7d3cb6b256b83
Абдель <AR.Eid@rendez-vous.ru>
2024-10-25 15:20:06 +0300

    ДЗ спринт 13 (адаптер лиссинер и расширение класса)

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -1,9 +1,15 @@
 package com.practicum.playlistmaker
+import java.util.Date
 
 data class Track(
     val trackName: String, // Название композиции
     val artistName: String, // Имя исполнителя
     val trackTimeMillis: String, // Продолжительность трека
     val artworkUrl100: String, // Ссылка на изображение обложки
-    val trackId: Int// Гуид трека
+    val trackId: Int, // Гуид трека
+    val collectionName: String, // Название альбома
+    val releaseDate: Date, // Год релиза трека
+    val primaryGenreName: String, // Жанр трека
+    val country: String // Страна исполнителя
 )
+
